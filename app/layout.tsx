d783a7847622e1ee295c23dc75d0256c44f53ec5
Blair M McClenny <blairmmcclenny@Blairs-MBP.lan>
2024-05-23 11:09:08 -0700

    metadata wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
-import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { getSiteSettings } from "@/lib/queries/settings"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://loremipsum.com"),
-  title: {
-    template: `%s | Acme`,
-    default: "Acme",
-  },
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+export async function generateMetadata() {
+  const siteSettings = await getSiteSettings()
+
+  return {
+    metadataBase: siteSettings?.siteUrl && new URL(siteSettings?.siteUrl),
+    title: siteSettings?.siteName && {
+      default: siteSettings?.siteName,
+      template: `%s | ${siteSettings?.siteName}`,
+    },
+  }
 }
 
 export default function RootLayout({
